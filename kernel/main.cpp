d77c084f297b9f08d88ac42f0f67acd3a4f8d2c4
nonononoka <onon.kaori@gmail.com>
2023-09-19 21:13:20 +0900

    change kernel

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,4 +1,11 @@
-extern "C" void KernelMain(){ //extern "C" means that we can define function in C language.
+#include <cstdint>
+
+extern "C" void KernelMain(uint64_t frame_buffer_base,
+                           uint64_t frame_buffer_size){ //extern "C" means that we can define function in C language.
+    uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base); //cast to pointer type
+    for(uint64_t i = 0;i < frame_buffer_size;++i){
+        frame_buffer[i] = i % 256;
+    } 
     while(1) __asm__("hlt"); //inline assemblar inorder to embed assembly language into C language.
 }
 //"hlt means we want to stop CPU"
