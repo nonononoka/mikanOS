1419be9886fe0da5b57be511922716483365dfae
nonononoka <onon.kaori@gmail.com>
2023-09-21 20:44:49 +0900

    finish day05d

--- kernel/.main.d
+++ kernel/.main.d
@@ -4,4 +4,6 @@ main.o: main.cpp \
   /home/nonoka/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
   /home/nonoka/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
   /home/nonoka/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
   frame_buffer_config.hpp graphics.hpp font.hpp
---
+++ kernel/.newlib_support.d
@@ -0,0 +1,25 @@
+newlib_support.o: newlib_support.c \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/types.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/_ansi.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/newlib.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/_newlib_version.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/config.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/machine/ieeefp.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/features.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/cdefs.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/machine/_default_types.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/c++/v1/stddef.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/machine/_types.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/_types.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/_stdint.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/machine/endian.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/machine/_endian.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/select.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/_sigset.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/_timeval.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/timespec.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/_timespec.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/_pthreadtypes.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/sys/sched.h \
+  /home/nonoka/osbook/devenv/x86_64-elf/include/machine/types.h
